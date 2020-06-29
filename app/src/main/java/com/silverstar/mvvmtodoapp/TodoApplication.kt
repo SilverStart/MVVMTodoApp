b2601a9e8c9634e02117a1048e700ed18e0e6bcc
esahn <esahn@bitbucket.org>
2020-06-29 18:53:24 +0900

    Debugging Tool Flipper 추가 Database Plugin 추가

--- app/src/main/java/com/silverstar/mvvmtodoapp/TodoApplication.kt
+++ app/src/main/java/com/silverstar/mvvmtodoapp/TodoApplication.kt
@@ -1,7 +1,22 @@
 package com.silverstar.mvvmtodoapp
 
 import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.soloader.SoLoader
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TodoApplication : Application()
+class TodoApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+
+        if (BuildConfig.DEBUG) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(DatabasesFlipperPlugin(this))
+            client.start()
+        }
+    }
+}
